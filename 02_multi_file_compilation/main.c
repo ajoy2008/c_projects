812f89e76120f905c3cf31b6a5301fa89d36b572
Ajoy Kumar Samanta <ajoy2008.dev@gmail.com>
2017-08-27 19:35:46 +0530

    Multiple file compilation using Makefile

---
+++ 02_multi_file_compilation/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "my_math.h"
+int main(void)
+{
+  printf("my_add(20, 10) = %d\n", my_add(20,10));
+  printf("my_sub(20, 10) = %d\n", my_sub(20,10));
+  return 0;
+}
---
+++ 02_multi_file_compilation/my_add.c
@@ -0,0 +1,5 @@
+#include "my_math.h"
+int my_add(int a, int b)
+{
+  return(a+b);
+}
